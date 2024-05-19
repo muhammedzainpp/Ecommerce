2a926a0d2a8284ec150dfbda5b259bac7a3a7708
muhammedzainpp <muhammedzain848@gmail.com>
2024-05-19 11:37:41 +0530

    inject toast service and implement

--- Ecommerce.Web/Ecommerce.Web.Client/Shared/Admin/Categories/SaveCategory.razor.cs
+++ Ecommerce.Web/Ecommerce.Web.Client/Shared/Admin/Categories/SaveCategory.razor.cs
@@ -1,4 +1,5 @@
-﻿using Ecommerce.Web.Client.Services.Categories;
+﻿using Blazored.Toast.Services;
+using Ecommerce.Web.Client.Services.Categories;
 using Ecommerce.Web.Client.Services.Categories.Dtos;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
@@ -7,18 +8,22 @@ namespace Ecommerce.Web.Client.Shared.Admin.Categories;
 
 public partial class SaveCategory
 {
+    [Inject]
+    public required IToastService ToastService { get; set; } 
+
     [Inject] 
     public IJSRuntime JS { get; set; } = default!;
+
     [Inject]
     public ICategoryService CategoryService { get; set; } = default!;
+
     [SupplyParameterFromForm]
     public CategoryDto Category { get; set; } = new CategoryDto();
     public async Task SaveMainCategory() 
     {
          var response = await CategoryService.SaveMainCategory(Category);
-        if (!response.IsSuccess)
-             Console.WriteLine();
-
+         if (!response.IsSuccess)
+            ToastService.ShowError(String.Join(",", response.Errors));
     }
 
 }
