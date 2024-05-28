4977cd9aeedff3d2fd07e84e8f1334bd96f3b24c
muhammedzainpp <muhammedzain848@gmail.com>
2024-05-28 17:14:10 +0530

    changes item entity

--- Ecommerce.Web.Domain/Entities/Item.cs
+++ Ecommerce.Web.Domain/Entities/Item.cs
@@ -5,17 +5,29 @@ namespace Ecommerce.Web.Domain.Entities;
 public class Item : EntityBase
 {
     public Product Product { get; private set; } = default!;
-    public int ProductId { get; set; }
+    public int ProductId { get;private set; }
     public string ImageUrl { get; private set; } = default!;
     public Money Price { get; private set; } = default!;
-    public int Quantity { get; set; }
-    public static Item Create(int productId,Money price,string imageUrl)
+    public int Quantity { get;private set; }
+    public static Item Create(int productId,Money price,string imageUrl,int quantity)
     {
         return new Item()
         {
             ProductId = productId,
             Price = price,
-            ImageUrl = imageUrl
+            ImageUrl = imageUrl,
+            Quantity = quantity
         };
+    }
+
+    public static Item Update(Item item,int productId, Money price, string imageUrl, int quantity)
+    {
+        item.ProductId = productId;
+        item.Price = price;
+        item.ImageUrl = imageUrl;
+        item.Quantity = quantity;
+
+        return item;
+    
     }
 }
