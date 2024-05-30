b16b6ca15aec3e5edb656a82e9f5a9456fa78843
muhammedzainpp <muhammedzain848@gmail.com>
2024-05-30 19:26:35 +0530

    Change Image Url string To File Input

--- Ecommerce.Web/Ecommerce.Web.Client/Shared/Admin/Categories/SaveSubCategory.razor.cs
+++ Ecommerce.Web/Ecommerce.Web.Client/Shared/Admin/Categories/SaveSubCategory.razor.cs
@@ -1,11 +1,17 @@
 ﻿using Ecommerce.Web.Client.Services.Categories;
 using Ecommerce.Web.Client.Services.Categories.Dtos;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace Ecommerce.Web.Client.Shared.Admin.Categories;
 
 public partial class SaveSubCategory
 {
+    [Inject]
+    public IConfiguration config { get; set; } = default!;
+    private long maxFileSize = 1024 * 1024 * 3;
+    private int maxAllowedFiles = 5;
+    private List<string> errors = new();
     [Inject]
     public ICategoryService CategoryService { get; set; } = default!;
 
@@ -20,6 +26,47 @@ public partial class SaveSubCategory
     public async Task SaveSubCategoryAsync()
     {
         await CategoryService.SaveMainCategory(SubCategory);
+    }
+
+    private async Task LoadFiles(InputFileChangeEventArgs e)
+    {
+        errors.Clear();
+
+        if (e.FileCount > maxAllowedFiles)
+        {
+            errors.Add(
+                $"Error: Attempting to upload {e.FileCount} files, but only {maxAllowedFiles} files are allowed"
+            );
+            return;
+        }
+
+        foreach (var file in e.GetMultipleFiles(maxAllowedFiles))
+        {
+            try
+            {
+
+
+
+                string randomFileName = Path.GetRandomFileName();
+                string originalFileExtension = Path.GetExtension(file.Name);
+                string newFileName = Path.ChangeExtension(randomFileName, originalFileExtension);
+
+                //Combines three parts into a complete file path, string path = Path.Combine(Root directory, "sub-directory", newFileName);
+                string path = Path.Combine(
+                    config.GetValue<string>("FileStorage")!,
+                    "tcorey",
+                    newFileName);
+
+                Directory.CreateDirectory(Path.Combine(config.GetValue<string>("FileStorage")!, "tcorey"));
+
+                await using FileStream fs = new(path, FileMode.Create);
+                await file.OpenReadStream(maxFileSize).CopyToAsync(fs);
+            }
+            catch (Exception ex)
+            {
+                errors.Add($"File: {file.Name} Error: {ex.Message}");
+            }
+        }
     }
 
 }
--- Ecommerce.Web/Ecommerce.Web.Client/Shared/Admin/Products/SaveProduct.razor.cs
+++ Ecommerce.Web/Ecommerce.Web.Client/Shared/Admin/Products/SaveProduct.razor.cs
@@ -3,11 +3,20 @@ using Ecommerce.Web.Client.Services.Categories.Dtos;
 using Ecommerce.Web.Client.Services.Products;
 using Ecommerce.Web.Client.Services.Products.Dtos;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Ecommerce.Web.Client.Shared.Admin.Products;
 
 public partial class SaveProduct
 {
+    private long maxFileSize = 1024 * 1024 * 3;
+    private int maxAllowedFiles = 5;
+    private List<string> errors = new();
+
+
+    [Inject]
+    public IConfiguration config { get; set; } = default!;
     public int CategoryId { get; set; }
 
     [Inject]
@@ -32,5 +41,46 @@ public partial class SaveProduct
     public async void SaveProductAsync()
     {       
         await ProductService.SaveProduct(Product);
+    }
+
+    private async Task LoadFiles(InputFileChangeEventArgs e)
+    {
+        errors.Clear();
+
+        if (e.FileCount > maxAllowedFiles)
+        {
+            errors.Add(
+                $"Error: Attempting to upload {e.FileCount} files, but only {maxAllowedFiles} files are allowed"
+            );
+            return;
+        }
+
+        foreach (var file in e.GetMultipleFiles(maxAllowedFiles))
+        {
+            try
+            {
+
+
+
+                string randomFileName = Path.GetRandomFileName();
+                string originalFileExtension = Path.GetExtension(file.Name);
+                string newFileName = Path.ChangeExtension(randomFileName, originalFileExtension);
+
+                //Combines three parts into a complete file path, string path = Path.Combine(Root directory, "sub-directory", newFileName);
+                string path = Path.Combine(
+                    config.GetValue<string>("FileStorage")!,
+                    "tcorey",
+                    newFileName);
+
+                Directory.CreateDirectory(Path.Combine(config.GetValue<string>("FileStorage")!, "tcorey"));
+
+                await using FileStream fs = new(path, FileMode.Create);
+                await file.OpenReadStream(maxFileSize).CopyToAsync(fs);
+            }
+            catch (Exception ex)
+            {
+                errors.Add($"File: {file.Name} Error: {ex.Message}");
+            }
+        }
     }
 }
